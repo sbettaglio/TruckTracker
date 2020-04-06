b3eefe5bd5aa1f6c0e4ddb6deca0d8703952a894
Stefan Bettaglio <sbettaglio@gmail.com>
2020-04-05 22:15:08 -0400

    new carrier registration template

--- ClientApp/src/pages/NewCarrier.jsx
+++ ClientApp/src/pages/NewCarrier.jsx
@@ -1,43 +1,133 @@
 import React from 'react'
-
+import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap'
 const NewCarrier = () => {
   return (
     <>
       <div>
         <h1>Add New Carrier</h1>
       </div>
-      <main>
-        <form>
-          <section>
-            <p>Carrier Name</p>
-            <input type="text" name="carrierName"></input>
-          </section>
-          <section>
-            <p>MC Number</p>
-            <input type="number" name="mcNumber"></input>
-          </section>
-          <section>
-            <p>Primary Contact</p>
-            <input type="test" name="primaryContact"></input>
-          </section>
-          <section>
-            <p>Phone</p>
-            <input type="tel" name="phone"></input>
-          </section>
-          <section>
-            <p>email</p>
-            <input type="email" name="email"></input>
-          </section>
-          <section>
-            <p>Home State</p>
-            <input type="text" name="homeState"></input>
-          </section>
-          <section>
-            <p>Valid Insurance</p>
-            <input type="checkbox" name="validInsurance"></input>
-          </section>
-          <button>Save Carrier</button>
-        </form>
+      <main className="registration">
+        <Form>
+          <Row form>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="carrierName">Carrier Name</Label>
+                <Input
+                  type="text"
+                  name="carrierName"
+                  id="carrierName"
+                  placeholder="Carrier name goes here"
+                />
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="exampleMC">MC Number</Label>
+                <Input
+                  type="number"
+                  name="mCNumber"
+                  id="exampleMC"
+                  placeholder="123456"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="examplePrimaryContact">Primary Contact</Label>
+                <Input
+                  type="text"
+                  name="primaryContact"
+                  id="examplePrimaryContact"
+                  placeholder="John Doe"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="examplePhone">Phone</Label>
+                <Input
+                  type="tel"
+                  name="phone"
+                  id="examplePhone"
+                  placeholder="555-867-5309"
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row form>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="exampleEmail">Email</Label>
+                <Input type="email" name="email" id="exampleEmail" />
+              </FormGroup>
+            </Col>
+            <Col md={4}>
+              <FormGroup>
+                <Label for="exampleState">Home State</Label>
+                <Input type="select" name="state" id="exampleState">
+                  <option value="AL">Alabama</option>
+                  <option value="AK">Alaska</option>
+                  <option value="AZ">Arizona</option>
+                  <option value="AR">Arkansas</option>
+                  <option value="CA">California</option>
+                  <option value="CO">Colorado</option>
+                  <option value="CT">Connecticut</option>
+                  <option value="DE">Delaware</option>
+                  <option value="DC">District Of Columbia</option>
+                  <option value="FL">Florida</option>
+                  <option value="GA">Georgia</option>
+                  <option value="HI">Hawaii</option>
+                  <option value="ID">Idaho</option>
+                  <option value="IL">Illinois</option>
+                  <option value="IN">Indiana</option>
+                  <option value="IA">Iowa</option>
+                  <option value="KS">Kansas</option>
+                  <option value="KY">Kentucky</option>
+                  <option value="LA">Louisiana</option>
+                  <option value="ME">Maine</option>
+                  <option value="MD">Maryland</option>
+                  <option value="MA">Massachusetts</option>
+                  <option value="MI">Michigan</option>
+                  <option value="MN">Minnesota</option>
+                  <option value="MS">Mississippi</option>
+                  <option value="MO">Missouri</option>
+                  <option value="MT">Montana</option>
+                  <option value="NE">Nebraska</option>
+                  <option value="NV">Nevada</option>
+                  <option value="NH">New Hampshire</option>
+                  <option value="NJ">New Jersey</option>
+                  <option value="NM">New Mexico</option>
+                  <option value="NY">New York</option>
+                  <option value="NC">North Carolina</option>
+                  <option value="ND">North Dakota</option>
+                  <option value="OH">Ohio</option>
+                  <option value="OK">Oklahoma</option>
+                  <option value="OR">Oregon</option>
+                  <option value="PA">Pennsylvania</option>
+                  <option value="RI">Rhode Island</option>
+                  <option value="SC">South Carolina</option>
+                  <option value="SD">South Dakota</option>
+                  <option value="TN">Tennessee</option>
+                  <option value="TX">Texas</option>
+                  <option value="UT">Utah</option>
+                  <option value="VT">Vermont</option>
+                  <option value="VA">Virginia</option>
+                  <option value="WA">Washington</option>
+                  <option value="WV">West Virginia</option>
+                  <option value="WI">Wisconsin</option>
+                  <option value="WY">Wyoming</option>
+                </Input>
+              </FormGroup>
+            </Col>
+          </Row>
+          <FormGroup check>
+            <Input
+              type="checkbox"
+              name="validInsurance"
+              id="exampleInsurance"
+            />
+            <Label for="exampleInsurance" check>
+              Valid Insurance
+            </Label>
+          </FormGroup>
+          <Button>Save Carrier</Button>
+        </Form>
       </main>
     </>
   )
