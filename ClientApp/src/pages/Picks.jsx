f624cac95b99cf82622b19c8b45bb5d9331961b9
Stefan Bettaglio <sbettaglio@gmail.com>
2020-04-07 19:46:35 -0400

    registration responsive

--- ClientApp/src/pages/Picks.jsx
+++ ClientApp/src/pages/Picks.jsx
@@ -4,16 +4,16 @@ import LoadTable from '../components/LoadTable/LoadTable'
 const Picks = () => {
   return (
     <>
-      <div>
+      <div className="title-div">
         <h1>Today's Picks</h1>
       </div>
       <main className="loads-main">
         <LoadTable
           slot1="Id"
           slot2="P/U City"
-          slot3="P/U Date"
+          slot3="P/U Appointment"
           slot4="D/O City"
-          slot5="D/O DAte"
+          slot5="D/O Appointment"
         />
       </main>
     </>
