2d7019f66ebd595f2006272e4156b6c268745744
Stefan Bettaglio <sbettaglio@gmail.com>
2020-04-09 20:06:03 -0400

    added primariy contact to carrier table

---
+++ Migrations/20200410000415_AddedPrimaryContact.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TruckTracker.Migrations
+{
+    public partial class AddedPrimaryContact : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PrimaryContact",
+                table: "Carriers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PrimaryContact",
+                table: "Carriers");
+        }
+    }
+}
--- Models/Carrier.cs
+++ Models/Carrier.cs
@@ -8,6 +8,7 @@ namespace TruckTracker.Models
     public string CarrierName { get; set; }
     public int MCNumber { get; set; }
     public string PhoneNumber { get; set; }
+    public string PrimaryContact { get; set; }
     public string Email { get; set; }
     public string HomeState { get; set; }
     public bool ValidInsurance { get; set; }
