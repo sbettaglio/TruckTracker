629e2f5a536063b3fdfb101c6fd2f66407cf4e2e
Stefan Bettaglio <sbettaglio@gmail.com>
2020-04-17 12:13:58 -0400

    user home cards responsive

--- ClientApp/src/components/UserHomeComponents/HomeTable.jsx
+++ ClientApp/src/components/UserHomeComponents/HomeTable.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Table, Container } from 'reactstrap'
+import { Table, Container, Row, Col } from 'reactstrap'
 import HomeCard from './HomeCard'
 const HomeTable = () => {
   return (
     <>
       <Container>
-        <HomeCard text="Today's Picks" link="picks" />
-        <HomeCard text="Today's Drops" link="drops" />
-        <HomeCard text="Late Trucks" link="late" />
-        <HomeCard text="Available Loads" link="available" />
-        <HomeCard text="Create New Load" link="create" />
-        <HomeCard text="Add New Carrier" link="newCarrier" />
-        <HomeCard text="Search" link="search" />
+        <Row>
+          <Col sm={1} md={6} xl={4}>
+            <HomeCard text="Today's Picks" link="picks" />
+          </Col>
+          <Col sm={1} md={6} xl={4}>
+            <HomeCard text="Today's Drops" link="drops" />
+          </Col>
+          <Col sm={1} md={6} xl={4}>
+            <HomeCard text="Late Trucks" link="late" />
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={1} md={6} xl={4}>
+            <HomeCard text="Available Loads" link="available" />
+          </Col>
+
+          <Col sm={1} md={6} xl={4}>
+            <HomeCard text="Create New Load" link="create" />
+          </Col>
+          <Col sm={1} md={6} xl={4}>
+            <HomeCard text="Add New Carrier" link="newCarrier" />
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={1} md={12} xl={{ size: 6, offset: 3 }}>
+            <HomeCard text="Search" link="search" />
+          </Col>
+        </Row>
       </Container>
     </>
   )
