9afd97a1b6d1bff772c02eb83c6fff52534ecc4a
Stefan Bettaglio <sbettaglio@gmail.com>
2020-04-16 20:03:40 -0400

    interactive navbar sticky

--- ClientApp/src/components/NavMenu/CustomNav.jsx
+++ ClientApp/src/components/NavMenu/CustomNav.jsx
@@ -16,58 +16,75 @@ import {
 
 const CustomNav = props => {
   const [isOpen, setIsOpen] = useState(false)
+  const [collapsed, setCollapsed] = useState(true)
 
+  const toggleNavbar = () => setCollapsed(!collapsed)
   const toggle = () => setIsOpen(!isOpen)
   return (
     <header className="bg-primary">
-      <Navbar color="primary" dark expand="md">
+      <Navbar color="primary" dark expand="md" sticky="top">
         <NavbarBrand href="/">Truck Tracker</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
+          <Nav navbar>
             <UncontrolledDropdown nav inNavbar>
               <DropdownToggle nav caret>
-                Loads
+                Menu
               </DropdownToggle>
               <DropdownMenu right>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    Loads
+                  </DropdownToggle>
+                  <DropdownMenu left>
+                    <DropdownItem>
+                      <NavItem>
+                        <NavLink href="/picks">Today's Picks</NavLink>
+                      </NavItem>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <NavItem>
+                        <NavLink href="/drops">Today's Drops</NavLink>
+                      </NavItem>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <NavItem>
+                        <NavLink href="/late">Late Trucks</NavLink>
+                      </NavItem>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <NavItem>
+                        <NavLink href="/available">Available Loads</NavLink>
+                      </NavItem>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <NavItem>
+                        <NavLink href="/create">Create New Load</NavLink>
+                      </NavItem>
+                    </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>Close</DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
                 <DropdownItem>
                   <NavItem>
-                    <NavLink href="/picks">Today's Picks</NavLink>
+                    <NavLink href="/search">Search</NavLink>
                   </NavItem>
                 </DropdownItem>
                 <DropdownItem>
                   <NavItem>
-                    <NavLink href="/drops">Today's Drops</NavLink>
+                    <NavLink href="/newCarrier">New Carrier</NavLink>
                   </NavItem>
                 </DropdownItem>
                 <DropdownItem>
                   <NavItem>
-                    <NavLink href="/late">Late Trucks</NavLink>
-                  </NavItem>
-                </DropdownItem>
-                <DropdownItem>
-                  <NavItem>
-                    <NavLink href="/available">Available Loads</NavLink>
-                  </NavItem>
-                </DropdownItem>
-                <DropdownItem>
-                  <NavItem>
-                    <NavLink href="/create">Create New Load</NavLink>
+                    <NavLink href="/">Log Out</NavLink>
                   </NavItem>
                 </DropdownItem>
                 <DropdownItem divider />
                 <DropdownItem>Close</DropdownItem>
               </DropdownMenu>
             </UncontrolledDropdown>
-            <NavItem>
-              <NavLink href="/search">Search</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/newCarrier">New Carrier</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/">Log Out</NavLink>
-            </NavItem>
           </Nav>
         </Collapse>
       </Navbar>
